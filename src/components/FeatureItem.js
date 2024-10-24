f417a548affde11be005a88dca18c3239e0b067d
Sandra C. <sandra.colomer@outlook.fr>
2024-10-24 16:19:41 +0200

    Add feature items to Home page

---
+++ src/components/FeatureItem.js
@@ -0,0 +1,9 @@
+const FeatureItem = ({ imgSrc, imgAlt, title, description }) => (
+    <div className="feature-item">
+      <img src={imgSrc} alt={imgAlt} className="feature-icon" />
+      <h3 className="feature-item-title">{title}</h3>
+      <p>{description}</p>
+    </div>
+  );
+  
+  export default FeatureItem;
