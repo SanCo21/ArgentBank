929044afc2d56548df303520ae406925e35144a8
Sandra C. <sandra.colomer@outlook.fr>
2024-10-31 16:25:22 +0100

    Refactor EditNameForm component to include Redux state management and update user name

--- src/components/EditName.js
+++ src/components/EditName.js
@@ -1,69 +1,72 @@
 import React, { useState } from "react";
-import PropTypes from "prop-types";
+import { useDispatch } from "react-redux";
+import { updateUserName } from "../reducers/userReducer";
 
 const EditNameForm = ({
-  currentUserName,
-  currentFirstName,
-  currentLastName,
-  handleSave,
-  handleCancel,
+    currentUserName,
+    currentFirstName,
+    currentLastName,
+    handleSave,
+    handleCancel,
 }) => {
-  const [userName, setUserName] = useState(currentUserName);
+    const [userName, setUserName] = useState(currentUserName);
+    const dispatch = useDispatch();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    handleSave(userName);
-  };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        handleSave(userName);
+        dispatch(updateUserName(userName)); // Update the user name in the Redux store
+    };
 
-  return (
-    <section className="modal">
-      <h2>Edit user info</h2>
-      <form onSubmit={handleSubmit} className="edit-form">
-        <div className="edit-input-wrapper">
-          <label>User name:</label>
-          <input
-            type="text"
-            value={userName}
-            onChange={(e) => setUserName(e.target.value)}
-          />
-        </div>
-        <div className="edit-input-wrapper">
-          <label> First name:</label> {/* Displaying read-only */}
-          <input
-            type="text"
-            value={currentFirstName}
-            disabled
-            // onChange={(e) => setFirstName(e.target.value)}
-          />
-        </div>
-        <div className="edit-input-wrapper">
-          <label>Last name:</label> {/* Displaying read-only */}
-          <input
-            type="text"
-            value={currentLastName}
-            disabled
-            // onChange={(e) => setLastName(e.target.value)}
-          />
-        </div>
-        <div className="button-group">
-          <button type="submit" className="modal-button">
-            Save
-          </button>
-          <button type="button" onClick={handleCancel} className="modal-button">
-            Cancel
-          </button>
-        </div>
-      </form>
-    </section>
-  );
+    return (
+        <section className="modal">
+        <h2>Edit user info</h2>
+        <form onSubmit={handleSubmit} className="edit-form">
+            <div className="edit-input-wrapper">
+            <label>User name:</label>
+            <input
+                type="text"
+                value={userName}
+                onChange={(e) => setUserName(e.target.value)}
+            />
+            </div>
+            <div className="edit-input-wrapper">
+            <label> First name:</label> {/* Displaying read-only */}
+            <input
+                type="text"
+                value={currentFirstName}
+                disabled
+                // onChange={(e) => setFirstName(e.target.value)}
+            />
+            </div>
+            <div className="edit-input-wrapper">
+            <label>Last name:</label> {/* Displaying read-only */}
+            <input
+                type="text"
+                value={currentLastName}
+                disabled
+                // onChange={(e) => setLastName(e.target.value)}
+            />
+            </div>
+            <div className="button-group">
+            <button type="submit" className="modal-button">
+                Save
+            </button>
+            <button type="button" onClick={handleCancel} className="modal-button">
+                Cancel
+            </button>
+            </div>
+        </form>
+        </section>
+    );
 };
 
-EditNameForm.propTypes = {
-    // currentUserName: PropTypes.string.isRequired,
-    currentFirstName: PropTypes.string.isRequired,
-    currentLastName: PropTypes.string.isRequired,
-    handleSave: PropTypes.func.isRequired,
-    handleCancel: PropTypes.func.isRequired,
-};
+// EditNameForm.propTypes = {
+//   // currentUserName: PropTypes.string.isRequired,
+//   currentFirstName: PropTypes.string.isRequired,
+//   currentLastName: PropTypes.string.isRequired,
+//   handleSave: PropTypes.func.isRequired,
+//   handleCancel: PropTypes.func.isRequired,
+// };
 
 export default EditNameForm;
--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,21 +1,26 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   user: null,
 };
 
 const userSlice = createSlice({
-  name: 'user',
+  name: "user",
   initialState,
   reducers: {
     setUser(state, action) {
       state.user = action.payload;
     },
-    signOut(state) { 
-        state.user = null; 
+    signOut(state) {
+      state.user = null;
+    },
+    updateUserName(state, action) {
+      if (state.user) {
+        state.user.userName = action.payload;
+      }
     },
   },
 });
 
-export const { setUser, signOut } = userSlice.actions; 
+export const { setUser, signOut, updateUserName } = userSlice.actions;
 export default userSlice.reducer;
