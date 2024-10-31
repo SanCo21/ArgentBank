3751ce3983ae646b0af3042a54c631f44e89621f
Sandra C. <sandra.colomer@outlook.fr>
2024-10-31 19:10:12 +0100

    Login validation

--- src/components/InputWrapper.js
+++ src/components/InputWrapper.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React from "react";
 
-const InputWrapper = ({ label, type, id, value, onChange }) => (
-  <div className="input-wrapper">
-    <label htmlFor={id}>{label}</label>
-    <input type={type} id={id} value={value} onChange={onChange} />
-  </div>
-);
+const InputWrapper = ({ label, type, id, value, onChange, isValid }) => {
+  return (
+    <div className={`input-wrapper ${isValid ? 'valid' : ''}`}>
+      <label htmlFor={id}>{label}</label>
+      <input type={type} id={id} value={value} onChange={onChange} />
+      {isValid && <span className="checkmark">✔️</span>}
+    </div>
+  );
+};
 
 export default InputWrapper;
--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { setUser } from "../reducers/userReducer";
 import { login, fetchUserData } from "../API/userService";
 import InputWrapper from "../components/InputWrapper";
 import CheckboxWrapper from "../components/CheckboxWrapper";
+import { FaCheck } from "react-icons/fa";
 
 const SignIn = () => {
   const dispatch = useDispatch();
@@ -13,34 +14,76 @@ const SignIn = () => {
   const [email, setEmail] = useState(""); // Change username to email
   const [password, setPassword] = useState("");
   const [rememberMe, setRememberMe] = useState(false);
+  // const [error, setError] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+  const [isEmailValid, setIsEmailValid] = useState(false);
+  const [isPasswordValid, setIsPasswordValid] = useState(false);
+
+  // Regex with letters, numbers, dots, underscores, and hyphens in local part
+  // and letters, numbers, and hyphens in domain part and a length of 2 to 4 letters in the top-level domain
+  const validateEmail = (email) => {
+    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return regex.test(email);
+  };
+  // Regex with at least 8 characters and one number
+  const validatePassword = (password) => {
+    const regex = /^(?=.*\d)[a-zA-Z\d]{8,}$/;
+    return regex.test(password);
+  };
+
+  useEffect(() => {
+    // Check if all fields are filled and valid
+    if (email && !validateEmail(email)) {
+      setEmailError("Invalid email format");
+      setIsEmailValid(false);
+    } else if (email && validateEmail(email)) {
+      setEmailError("");
+      setIsEmailValid(true);
+    } else {
+      setEmailError("");
+      setIsEmailValid(false);
+    }
+
+    if (password && !validatePassword(password)) {
+      setPasswordError(
+        "Password must be at least 8 characters long and contain at least one number"
+      );
+      setIsPasswordValid(false);
+    } else if (password && validatePassword(password)) {
+      setPasswordError("");
+      setIsPasswordValid(true);
+    } else {
+      setPasswordError("");
+      setIsPasswordValid(false);
+    }
+
+    if (validateEmail(email) && validatePassword(password)) {
+      setIsButtonDisabled(false);
+    } else {
+      setIsButtonDisabled(true);
+    }
+  }, [email, password]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    try {      
+    setEmailError("");
+    setPasswordError("");
+
+    try {
       const loginResponse = await login({ email, password }); // Change username to email
       const token = loginResponse.body.token;
-      const user = await fetchUserData(token);      
+      const user = await fetchUserData(token);
 
       // Dispatch user and token to Redux
-      dispatch(setUser( { user, token }));
-      navigate('/user'); // Redirect to User page after successful login
+      dispatch(setUser({ user, token }));
+      navigate("/user"); // Redirect to User page after successful login
     } catch (error) {
       console.error("Login failed:", error);
+      setEmailError("Login failed. Please check your credentials and try again.");
     }
   };
-
-  // const handleSubmit = async (e) => { 
-  //   e.preventDefault(); 
-  //   try { 
-  //     const loginData = { email, password }; // Change username to email 
-  //     console.log('Login Data:', loginData); // Log the data being sent 
-  //     const user = await login(loginData); 
-  //     dispatch(setUser(user)); 
-  //     navigate('/user'); // Redirect to User page after successful login
-  //   } catch (error) {  
-  //     console.error("Login failed:", error); 
-  //   } 
-  // };
 
   return (
     <main className="main bg-dark">
@@ -48,33 +91,51 @@ const SignIn = () => {
         <i className="fa fa-user-circle sign-in-icon"></i>
         <h1>Sign In</h1>
         <form onSubmit={handleSubmit}>
-          <InputWrapper
-            label="Email"
-            type="text"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <InputWrapper
-            label="Password"
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
+          {emailError && <p className="error-message">{emailError}</p>}
+          {passwordError && <p className="error-message">{passwordError}</p>}
+          <div
+            className={`input-wrapper ${!emailError && email ? "valid" : ""}`}
+          >
+            <InputWrapper
+              label="Email"
+              type="text"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            {isEmailValid && <FaCheck className="valid-check" />}
+          </div>
+          <div
+            className={`input-wrapper ${
+              !passwordError && password ? "valid" : ""
+            }`}
+          >
+            <InputWrapper
+              label="Password"
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            {isPasswordValid && <FaCheck className="valid-check" />}
+          </div>
           <CheckboxWrapper
             label="Remember me"
             id="remember-me"
             checked={rememberMe}
             onChange={(e) => setRememberMe(e.target.checked)}
           />
-          <button type="submit" className="sign-in-button">
+          <button
+            type="submit"
+            className="sign-in-button"
+            disabled={isButtonDisabled}
+          >
             Sign In
           </button>
         </form>
+        {/* {error && <p className="error-message">{error}</p>} */}
       </section>
     </main>
   );
 };
-
 export default SignIn;
