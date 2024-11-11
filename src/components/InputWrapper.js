b426a44db70ea373dd2a9dae7454f52caa4485f9
Sandra C. <sandra.colomer@outlook.fr>
2024-11-11 17:12:39 +0100

    update background image in CSS

--- src/components/InputWrapper.js
+++ src/components/InputWrapper.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const InputWrapper = ({ label, type, id, value, onChange, isValid }) => {
   return (
-    <div className={`input-wrapper ${isValid ? 'valid' : ''}`}>
+    <div className={`input-wrapper ${isValid ? "valid" : ""}`}>
       <label htmlFor={id}>{label}</label>
       <input type={type} id={id} value={value} onChange={onChange} />
     </div>
